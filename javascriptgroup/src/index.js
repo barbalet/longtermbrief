06523e22fe72abe0b8a7f18832a5d17f5c00f1ed
Tom Barbalet <barbalet@gmail.com>
2026-01-15 21:42:41 -0800

    Update to command line

--- javascriptgroup/src/index.js
+++
@@ -1,6 +0,0 @@
-import * as toolkit from './toolkit/toolkit.js';
-import * as entity from './entity/entity.js';
-import * as universe from './universe/universe.js';
-import * as sim from './sim/sim.js';
-console.log('ApeSDK JS baseline port loaded');
-console.log('Exports:', {toolkit:Object.keys(toolkit).length, entity:Object.keys(entity).length, universe:Object.keys(universe).length, sim:Object.keys(sim).length});
